65de6ab8575a5f9d97f29235ac4ffa7d03f5caf3
Gabriel Gigante <gabriel_gigante@outlook.com>
2021-04-03 12:18:21 -0300

    feat: base structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Ig.news</title>
+      </Head>
+
+      <h1>hello world</h1>
+    </>
+  )
+}
