9d25c2921a153c57ab1568f521180240b70a70c7
Gabriel Gigante <gabriel_gigante@outlook.com>
2021-04-03 21:39:31 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | Ig.news</title>
+        <title>Home | Ig.news</title>
       </Head>
 
-      <h1>hello world</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <div>
+            <span>👏Hey, welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+            <p>
+              Get access to all the publications <br />
+              <span>for $9.90 month</span>
+            </p>
+          </div>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
