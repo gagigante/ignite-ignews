ae4e748fe94ed881207b51c3ad8ee7e87e4fcc5e
Gabriel Gigante <gabriel_gigante@outlook.com>
2021-08-28 14:43:37 -0300

    feat: showing prismic posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss'
 
-export default function Posts() {
+type Post = {
+  slug: string
+  title: string
+  excerpt: string
+  updatedAt: string
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -15,23 +27,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Post title</strong>
-            <p>Um dos principais desafios para as arquiteturas Single tenant sempre foi a dificuldade para manter essa estrutura distribuída com muitas instâncias</p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Post title</strong>
-            <p>Um dos principais desafios para as arquiteturas Single tenant sempre foi a dificuldade para manter essa estrutura distribuída com muitas instâncias</p>
-          </a>
-
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Post title</strong>
-            <p>Um dos principais desafios para as arquiteturas Single tenant sempre foi a dificuldade para manter essa estrutura distribuída com muitas instâncias</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }
         </div>
       </main>
     </>
@@ -48,9 +50,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts: Post[] = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText( post.data.title),
+      excerpt: post.data.content.find((content: { type: string }) => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
