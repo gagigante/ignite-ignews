50cfc197faa740d9303df60508d9356c54a45101
Gabriel Gigante <gabriel_gigante@outlook.com>
2021-09-06 16:42:20 -0300

    feat: validating subscription

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from 'next'
 import Head from 'next/head'
-// import { getSession } from 'next-auth/client'
+import { getSession } from 'next-auth/client'
 import { RichText } from 'prismic-dom'
 
 import { getPrismicClient } from '../../services/prismic'
@@ -41,18 +41,23 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
-  // const session = await getSession({ req })
-  const { slug } = params;
+  const session = await getSession({ req })
+  const { slug } = params
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
   
-  // if (!session) {
-
-  // }
-
   const prismic = getPrismicClient(req)
   const response = await prismic.getByUID('publication', String(slug), {})
 
   const post = {
-    slug: response.uid,
+    slug,
     title: RichText.asText(response.data.title),
     content: RichText.asHtml(response.data.content),
     updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
